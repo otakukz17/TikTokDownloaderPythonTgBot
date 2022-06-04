6065efa3573d799b53dce4c983c40dd6c370326e
kydyr <otakukz17@gmail.com>
2022-06-04 14:43:02 +0600

    Add main functional

--- config.py
+++ config.py
@@ -1,10 +1,22 @@
 import os
 from aiogram.dispatcher import Dispatcher
 from aiogram import Bot
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, InlineQueryResultArticle, InputTextMessageContent
+
 
 TOKEN = os.getenv('BOT_TOKEN')
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
+BOT_URL = os.getenv('BOT_URL')
+API_HOST = os.getenv('API_HOST')
+API_KEY = os.getenv('API_KEY')
+
+workers = 4
+api = os.getenv('API_ID')
+hash = os.getenv('API_HASH')
+
+app = Client("BOT_Name", bot_token=TOKEN, api_id=api, api_hash=hash, workers=workers)
 
 HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 
--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import logging
+import os
+import re
+import shutil
+import time
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, InlineQueryResultArticle, InputTextMessageContent
+import requests
+
 from aiogram.utils.executor import start_webhook
 from aiogram import types
-from config import bot, dp, WEBHOOK_PATH, WEBHOOK_URL, WEBAPP_PORT, WEBAPP_HOST
+from config import bot, dp, WEBHOOK_PATH, WEBHOOK_URL, WEBAPP_PORT, WEBAPP_HOST, BOT_URL, API_HOST, API_KEY, app
 from db import database
+from progress_bar import progress
 
 
 async def on_startup(dispatcher):
@@ -27,14 +36,142 @@ async def read():
     return [next(result.values()) for result in results]
 
 
-@dp.message_handler(commands='start')
+@dp.message_handler(commands=['start', 'help'])
 async def start(message: types.Message):
-    await message.answer(text=f"Hi, I am **TikTok Downloader Bot**. \nI can download TikTok video without Watermark.\n"
-                              f"Just send me link on TikTok Video\n"
-                              f"__**Developer :**__ __@otakukz17__\n"
-                              "__**Language :**__ __Python__\n"
-                              "__**Framework :**__ __Aiogram",
-                         parse_mode='Markdown')
+    await message.reply(text=f"Hi, I am **TikTok Downloader Bot**. \nI can download TikTok video without Watermark.\n"
+                             f"Just send me link on TikTok Video\n"
+                             f"__**Developer :**__ __@otakukz17__\n"
+                             "__**Language :**__ __Python__\n"
+                             "__**Framework :**__ __Aiogram",
+                        parse_mode='Markdown')
+
+
+@app.on_message(
+    (filters.regex("http://") | filters.regex("https://")) & (filters.regex('tiktok') | filters.regex('douyin')))
+def tiktok_dl(client, message):
+    a = app.send_message(chat_id=message.chat.id,
+                         text='__Downloading File to the Server__')
+    link = re.findall(r'\bhttps?://.*[(tiktok|douyin)]\S+', message.text)[0]
+    link = link.split("?")[0]
+
+    url = "https://tiktok-downloader-download-tiktok-videos-without-watermark.p.rapidapi.com/vid/index"
+
+    querystring = {"url": link}
+
+    headers = {
+        "X-RapidAPI-Host": {API_HOST},
+        "X-RapidAPI-Key": {API_KEY}
+    }
+
+    response = requests.get(url, headers=headers, params=querystring).json()['data']['play']
+    directory = str(round(time.time()))
+    filename = str(int(time.time())) + '.mp4'
+    size = int(requests.head(response).headers['Content-length'])
+    total_size = "{:.2f}".format(int(size) / 1048576)
+    try:
+        os.mkdir(directory)
+    except:
+        pass
+    with requests.get(response, timeout=(50, 10000), stream=True) as r:
+        r.raise_for_status()
+        with open(f'./{directory}/{filename}', 'wb') as f:
+            chunk_size = 1048576
+            dl = 0
+            show = 1
+            for chunk in r.iter_content(chunk_size=chunk_size):
+                f.write(chunk)
+                dl = dl + chunk_size
+                percent = round(dl * 100 / size)
+                if percent > 100:
+                    percent = 100
+                if show == 1:
+                    try:
+                        a.edit(f'__**URL :**__ __{message.text}__\n'
+                               f'__**Total Size :**__ __{total_size} MB__\n'
+                               f'__**Download :**__ __{percent}%__\n')
+                    except:
+                        pass
+                    if percent == 100:
+                        show = 0
+        a.edit(f'__Downloaded to the server!\n'
+               f'Uploading to Telegram Now __')
+        start = time.time()
+        title = filename
+        app.send_document(chat_id=message.chat.id,
+                          document=f"./{directory}/{filename}",
+                          caption=f"**File: ** __{filename}__\n"
+                                  f"**Size :** __{total_size} MB__ \n\n"
+                                  f"__Uploaded by @{BOT_URL}__",
+                          file_name=f"{directory}",
+                          progress=progress,
+                          progress_args=(a, start, title))
+        a.delete()
+        try:
+            shutil.rmtree(directory)
+        except:
+            pass
+
+# @dp.message_handler(regexp='tiktok')
+# async def tiktok_dl(message: types.Message):
+#     a = await message.answer(text='__Downloading File to the Server__')
+#     link = await re.findall(r'\bhttps?://.*[(tiktok|douyin)]\S+', message.text)[0]
+#     link = await link.split("?")[0]
+#
+    # url = "https://tiktok-downloader-download-tiktok-videos-without-watermark.p.rapidapi.com/vid/index"
+    #
+    # querystring = {"url": link}
+    #
+    # headers = {
+    #     "X-RapidAPI-Host": {API_HOST},
+    #     "X-RapidAPI-Key": {API_KEY}
+    # }
+#
+#     response = await requests.request("GET", url, headers=headers, params=querystring)
+#     directory = str(round(time.time()))
+#     filename = str(int(time.time())) + '.mp4'
+#     size = int(requests.head(response).headers['Content-length'])
+#     total_size = "{:.2f}".format(int(size) / 1048576)
+#     try:
+#         os.mkdir(directory)
+#     except:
+#         pass
+#     with requests.get(response, timeout=(50, 10000), stream=True) as r:
+#         r.raise_for_status()
+#         with open(f'./{directory}/{filename}', 'wb') as f:
+#             chunk_size = 1048576
+#             dl = 0
+#             show = 1
+#             for chunk in r.iter_content(chunk_size=chunk_size):
+#                 f.write(chunk)
+#                 dl = dl + chunk_size
+#                 percent = round(dl * 100 / size)
+#                 if percent > 100:
+#                     percent = 100
+#                 if show == 1:
+#                     try:
+#                         await a.edit_text(f'__**URL :**__ __{message.text}__\n'
+#                                           f'__**Total Size :**__ __{total_size} MB__\n'
+#                                           f'__**Download :**__ __{percent}%__\n')
+#                     except:
+#                         pass
+#                     if percent == 100:
+#                         show = 0
+#         await a.edit_text(f'__Downloaded to the server!\n'
+#                           f'Uploading to Telegram Now __')
+#         start = time.time()
+#         title = filename
+#         await message.reply_document(document=f"./{directory}/{filename}",
+#                                      caption=f"**File: ** __{filename}__\n"
+#                                              f"**Size :** __{total_size} MB__ \n\n"
+#                                              f"__Uploaded by @{BOT_URL}__",
+#                                      file_name=f"{directory}",
+#                                      progress=progress,
+#                                      progress_args=(a, start, title))
+#         await a.delete()
+#         try:
+#             shutil.rmtree(directory)
+#         except:
+#             pass
 
 
 # @dp.message_handler()
