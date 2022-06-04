e20e2f25274098b5c64a3efe2bdd0103473c0200
kydyr <otakukz17@gmail.com>
2022-06-04 12:52:40 +0600

    Fix Create Table in Postgres

--- db.py
+++ db.py
@@ -1,6 +1,4 @@
 from databases import Database
 from config import DB_URL
 database = Database(DB_URL)
-await database.connect()
-query = "CREATE TABLE messages (id SERIAL PRIMARY KEY,telegram_id INTEGER NOT NULL,text text NOT NULL);"
-await database.execute(query=query)
+
